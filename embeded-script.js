20429b1ba3f9e54b58f342ce8c5a4391d74c2a6a
Mitsuru Takigahira <mysticdoll@mysticdoll.com>
2017-05-25 15:04:48 +0900

    bug fix

--- embeded-script.js
+++ embeded-script.js
@@ -1,7 +1,20 @@
-let button = document.querySelector(`a[href="${location.pathname.replace(/pulls/g, "compare")}"]`)
-
-button.addEventListener("click", function(e) {
-  if(!window.confirm("作者にPull Requestを作成する許可を取りましたか？")) {
-    e.preventDefault();
+const attachEvent = (user, repo) => {
+  let button = document.querySelector(`a[href="/${user}/${repo}/compare"]`)
+  if(button) {
+    button.addEventListener("click", function(e) {
+      if(!window.confirm("作者にPull Requestを作成する許可を取りましたか？")) {
+        e.preventDefault();
+      }
+    });
   }
+};
+
+let [_, user, repo] = /^\/([^\/]+?)\/([^\/]+)\/{0,1}/.exec(location.pathname);
+attachEvent(user, repo);
+
+let mo = new MutationObserver(function() {
+  let [_, user, repo] = /^\/([^\/]+?)\/([^\/]+)\/{0,1}/.exec(location.pathname);
+  attachEvent(user, repo);
 });
+
+mo.observe(document.body, {childList: true, subtree:true})
